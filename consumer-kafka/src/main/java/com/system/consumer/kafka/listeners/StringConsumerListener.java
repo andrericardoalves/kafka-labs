0ff891ca871b39deb3b8d5826638f3848b97c934
Alves <andre-ricardo.alves@capgemini.com>
2022-07-30 18:07:24 +0100

    Adding TopicPartition

--- consumer-kafka/src/main/java/com/system/consumer/kafka/listeners/StringConsumerListener.java
+++ consumer-kafka/src/main/java/com/system/consumer/kafka/listeners/StringConsumerListener.java
@@ -2,20 +2,27 @@ package com.system.consumer.kafka.listeners;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StringConsumerListener {
 
-    @KafkaListener(groupId = "group-0", topics = "kafka-topic",
+    @KafkaListener(groupId = "group-0",
+            topicPartitions = {
+                @TopicPartition(topic = "kafka-topic", partitions = {"0"})
+            },
             containerFactory = "concurrentKafkaListenerContainerFactory")
     public void create(String message){
 
         log.info("CREATE ::: Receive message {} ", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "kafka-topic",
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                    @TopicPartition(topic = "kafka-topic", partitions = {"1"})
+            },
             containerFactory = "concurrentKafkaListenerContainerFactory")
     public void log(String message){
 
