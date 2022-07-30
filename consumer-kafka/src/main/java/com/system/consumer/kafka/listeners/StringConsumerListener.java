6bc7cb1670998e3b903531094ca21cd4100837b1
Alves <andre-ricardo.alves@capgemini.com>
2022-07-30 18:25:05 +0100

    Create Custom Annotation

--- consumer-kafka/src/main/java/com/system/consumer/kafka/listeners/StringConsumerListener.java
+++ consumer-kafka/src/main/java/com/system/consumer/kafka/listeners/StringConsumerListener.java
@@ -1,5 +1,6 @@
 package com.system.consumer.kafka.listeners;
 
+import com.system.consumer.kafka.custom.StringConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -8,29 +9,19 @@ import org.springframework.stereotype.Component;
 @Log4j2
 @Component
 public class StringConsumerListener {
-
-    @KafkaListener(groupId = "group-0",
-            topicPartitions = {
-                @TopicPartition(topic = "kafka-topic", partitions = {"0"})
-            },
-            containerFactory = "concurrentKafkaListenerContainerFactory")
+    @StringConsumerCustomListener(groupId = "group-1")
     public void create(String message){
 
         log.info("CREATE ::: Receive message {} ", message);
     }
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-                    @TopicPartition(topic = "kafka-topic", partitions = {"1"})
-            },
-            containerFactory = "concurrentKafkaListenerContainerFactory")
+    @StringConsumerCustomListener(groupId = "group-1")
     public void log(String message){
 
         log.info("LOG ::: Receive message {} ", message);
     }
 
-    @KafkaListener(groupId = "group-2", topics = "kafka-topic",
-            containerFactory = "concurrentKafkaListenerContainerFactory")
+    @StringConsumerCustomListener(groupId = "group-2")
     public void history(String message){
 
         log.info("HISTORY ::: Receive message {} ", message);
